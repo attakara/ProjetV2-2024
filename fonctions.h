81fc5fcf8c55bec0bff5786c975e40acecfcfb84
attakara <churchwill@cy-tech.fr>
2024-05-24 18:46:59 +0200

    Update fonctions.h

---
+++ fonctions.h
@@ -0,0 +1,31 @@
+#ifndef FONCTIONS_H
+#define FONCTIONS_H
+
+#include "structures.h"
+
+void ajouter_donnees_entrainement(Sportif *sportif, Entrainement entrainement);
+void afficher_historique_entrainement(Sportif *sportif, char *epreuve, char *date_debut, char *date_fin);
+void afficher_statistiques_performance(Sportif *sportif);
+void choisir_athletes_pour_JO(char *epreuve);
+void charger_donnees_entrainement(Sportif *sportif);
+void consulter_performance(Sportif *sportif, char *epreuve, char *date1, char *date2);
+void afficher_relais(char *date_relais);
+int verifier_relais_existant(char *date);
+int verifie_prenom(char *prenom);
+int date_valide(int jour, int mois, int annee);
+int verifie_date(char *date);
+int verifier_epreuve(const char *epreuve);
+int verifier_format_temps(int heures, int minutes, int secondes, int millisecondes);
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
